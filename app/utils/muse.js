554a0bc9c6c9de2f3c7349d766fae1cfec771b3f
jdpigeon <morrisondano@gmail.com>
2018-06-25 14:47:36 -0400

    Successful Muse streaming after running rename command

--- app/utils/muse.js
+++ app/utils/muse.js
@@ -7,11 +7,16 @@ const createStream = () => {
     let device = await bluetooth.requestDevice({
       filters: [{ services: [MUSE_SERVICE] }]
     });
+    console.log('Found Device: ', device);
     const gatt = await device.gatt.connect();
     const client = new MuseClient();
     await client.connect(gatt);
     await client.start();
+    console.log('Connected!')
     // Now do whatever with muse client...
+    client.eegReadings.subscribe(reading => {
+      console.log(reading);
+    });
   }
 
   noble.on("stateChange", state => {
