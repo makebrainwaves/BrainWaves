ed188606ea4333bebfc8c14a19ed0fb148a2e892
jdpigeon <morrisondano@gmail.com>
2018-07-09 16:53:50 -0400

    Converted SSVEP and N170 to new Redux-stateful pattern

--- app/utils/jspsych/timelines/n170.js
+++ app/utils/jspsych/timelines/n170.js
@@ -11,64 +11,65 @@ const params = {
   plugin_name: "callback_image_display"
 };
 
-// Directories containing stimuli
+// Default directories containing stimuli
 // Note: there's a weird issue where the fs readdir function reads from BrainWaves dir
 // while the timeline reads from Brainwaves/app. Currently removing 'app/' from path in timeline
 const facesDir = "./app/assets/face_house/faces/";
 const housesDir = "./app/assets/face_house/houses/";
 
-// Default callback
-const callback = value => console.log(value, new Date().getTime());
-
-export const n170Timeline = {
-  mainTimeline: ["welcome", "faceHouseProcedure", "end"], // array of trial and timeline ids
-  welcome: {
-    type: "callback_html_display",
-    id: "welcome",
-    stimulus: "Welcome to the experiment. Press any key to begin.",
-    post_trial_gap: 1000,
-    on_load: () => callback("start")
-  },
-  faceHouseProcedure: {
-    id: "faceHouseProcedure",
-    timeline: [
-      {
-        type: "callback_image_display",
-        stimulus: "./assets/face_house/fixation.jpg",
-        trial_duration: () => params.iti + Math.random() * params.jitter,
-      },
-      {
-        stimulus: jsPsych.timelineVariable("stimulus"),
-        on_load: jsPsych.timelineVariable("callback"),
-        type: params.plugin_name,
-        choices: ["f", "j"],
-        trial_duration: params.trial_duration,
-      }
-    ],
-    sample: {
-      type: "with-replacement",
-      size: params.n_trials
+export const buildN170Timeline = callback => ({
+  mainTimeline: ["welcome", "faceHouseTimeline", "end"], // array of trial and timeline ids
+  trials: {
+    welcome: {
+      type: "callback_html_display",
+      id: "welcome",
+      stimulus: "Welcome to the experiment. Press any key to begin.",
+      post_trial_gap: 1000,
+      on_load: () => callback("start")
     },
-    timeline_variables: readdirSync(facesDir)
-      .filter(filename => filename.includes("3"))
-      .map(filename => ({
-        stimulus: facesDir.replace("app/", "") + filename,
-        callback: () => callback("face")
-      }))
-      .concat(
-        readdirSync(housesDir)
-          .filter(filename => filename.includes("3"))
-          .map(filename => ({
-            stimulus: housesDir.replace("app/", "") + filename,
-            callback: () => callback("house")
-          }))
-      )
+    end: {
+      id: "end",
+      type: "callback_html_display",
+      stimulus: "Thanks for participating",
+      post_trial_gap: 500,
+      on_load: callback("stop")
+    }
   },
-  end: {
-    id: "end",
-    type: "callback_html_display",
-    stimulus: "Thanks for participating",
-    post_trial_gap: 500,
-    on_load: callback("stop")
+  timelines: {
+    faceHouseTimeline: {
+      id: "faceHouseTimeline",
+      timeline: [
+        {
+          type: "callback_image_display",
+          stimulus: "./assets/face_house/fixation.jpg",
+          trial_duration: () => params.iti + Math.random() * params.jitter
+        },
+        {
+          stimulus: jsPsych.timelineVariable("stimulusVar"),
+          on_load: jsPsych.timelineVariable("callbackVar"),
+          type: params.plugin_name,
+          choices: ["f", "j"],
+          trial_duration: params.trial_duration
+        }
+      ],
+      sample: {
+        type: "with-replacement",
+        size: params.n_trials
+      },
+      timeline_variables: readdirSync(facesDir)
+        .filter(filename => filename.includes("3"))
+        .map(filename => ({
+          stimulusVar: facesDir.replace("app/", "") + filename,
+          callbackVar: () => callback("face")
+        }))
+        .concat(
+          readdirSync(housesDir)
+            .filter(filename => filename.includes("3"))
+            .map(filename => ({
+              stimulusVar: housesDir.replace("app/", "") + filename,
+              callbackVar: () => callback("house")
+            }))
+        )
+    }
   }
-};
+});
