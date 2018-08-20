97336c42be40f2d5c3b48321196782b0d9698d19
jdpigeon <morrisondano@gmail.com>
2018-08-20 18:24:57 -0400

    Added epic middleware to prod version

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -2,12 +2,16 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { createHashHistory } from "history";
+import { createEpicMiddleware } from "redux-observable";
+import rootEpic from "../epics";
+
 import { routerMiddleware } from "react-router-redux";
 import rootReducer from "../reducers";
 
 const history = createHashHistory();
+const epicMiddleware = createEpicMiddleware(rootEpic);
 const router = routerMiddleware(history);
-const enhancer = applyMiddleware(thunk, router);
+const enhancer = applyMiddleware(thunk, router, epicMiddleware);
 
 function configureStore(initialState?: any) {
   return createStore(rootReducer, initialState, enhancer);
