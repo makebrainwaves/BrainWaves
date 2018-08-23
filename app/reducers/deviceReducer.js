941eddec7265175dcc66c291fb5d09efe9bf2593
jdpigeon <morrisondano@gmail.com>
2018-08-22 20:29:03 -0400

    Fixed bug where emotiv datas wouldn't load because of no deviceType

--- app/reducers/deviceReducer.js
+++ app/reducers/deviceReducer.js
@@ -37,7 +37,7 @@ const initialState = {
   deviceAvailability: DEVICE_AVAILABILITY.NONE,
   rawObservable: null,
   signalQualityObservable: null,
-  deviceType: DEVICES.NONE
+  deviceType: DEVICES.EMOTIV
 };
 
 export default function device(
