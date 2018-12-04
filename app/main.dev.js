2bdc775c6e5d64860a5360045c69e4afddac7e01
jdpigeon <morrisondano@gmail.com>
2018-12-04 07:45:10 -0500

    v0.7.1

--- app/main.dev.js
+++ app/main.dev.js
@@ -91,6 +91,7 @@ app.on('ready', async () => {
   });
 
   mainWindow.setMenu(null);
+  // mainWindow.toggleDevTools();
   // const menuBuilder = new MenuBuilder(mainWindow);
   // menuBuilder.buildMenu();
 });
