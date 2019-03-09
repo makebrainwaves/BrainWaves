5f4a945c7bdf0ee7c5aac45df1212afb5488304a
jdpigeon <morrisondano@gmail.com>
2019-03-09 16:52:37 -0500

    Added explicit session id to inject markers

--- app/utils/eeg/emotiv.js
+++ app/utils/eeg/emotiv.js
@@ -3,29 +3,32 @@
  * an RxJS Observable of raw EEG data
  *
  */
-import { fromEvent } from "rxjs";
-import { map, withLatestFrom, share } from "rxjs/operators";
-import { addInfo, epoch, bandpassFilter } from "@neurosity/pipes";
-import { toast } from "react-toastify";
-import { parseEmotivSignalQuality } from "./pipes";
+import { fromEvent } from 'rxjs';
+import { map, withLatestFrom, share } from 'rxjs/operators';
+import { addInfo, epoch, bandpassFilter } from '@neurosity/pipes';
+import { toast } from 'react-toastify';
+import { parseEmotivSignalQuality } from './pipes';
 import {
   USERNAME,
   PASSWORD,
   CLIENT_ID,
   CLIENT_SECRET,
   LICENSE_ID
-} from "../../../keys";
-import { EMOTIV_CHANNELS, PLOTTING_INTERVAL } from "../../constants/constants";
-import Cortex from "./cortex";
+} from '../../../keys';
+import { EMOTIV_CHANNELS, PLOTTING_INTERVAL } from '../../constants/constants';
+import Cortex from './cortex';
 
 // Creates the Cortex object from SDK
 const verbose = process.env.LOG_LEVEL || 1;
 const options = { verbose };
 const client = new Cortex(options);
+// Used to explictly link event markers to session
+let sessionID = null;
 
 // Gets a list of available Emotiv devices
 export const getEmotiv = async () => {
   const devices = await client.queryHeadsets();
+  devices.push([{ id: 'Emotiv-debug' }]);
   return devices;
 };
 
@@ -46,9 +49,10 @@ export const connectToEmotiv = device =>
       toast.error(`Authentication failed. ${err}`);
       return err;
     })
+    .then(() => client.getLicenseInfo())
     .then(() =>
       client.createSession({
-        status: "active",
+        status: 'active',
         headset: device.id
       })
     )
@@ -58,8 +62,10 @@ export const connectToEmotiv = device =>
     })
     .then(session => {
       if (session.headset === undefined) {
-        return new Error("Session does not exist");
+        return new Error('Session does not exist');
       }
+      sessionID = session.id;
+
       return {
         name: session.headset.id,
         samplingRate: session.headset.settings.eegRate,
@@ -72,22 +78,22 @@ export const connectToEmotiv = device =>
     });
 
 export const disconnectFromEmotiv = async () => {
-  const sessionStatus = await client.updateSession({ status: "close" });
+  const sessionStatus = await client.updateSession({ status: 'close' });
   return sessionStatus;
 };
 
 // Returns an observable that will handle both connecting to Client and providing a source of EEG data
 export const createRawEmotivObservable = async () => {
-  const subs = await client.subscribe({ streams: ["eeg", "dev"] });
+  const subs = await client.subscribe({ streams: ['eeg', 'dev'] });
   if (!subs[0].eeg) {
     toast.error(`Subscription to Session data failed`);
   }
-  return fromEvent(client, "eeg").pipe(map(createEEGSample));
+  return fromEvent(client, 'eeg').pipe(map(createEEGSample));
 };
 
 // Creates an observable that will epoch, filter, and add signal quality to EEG stream
 export const createEmotivSignalQualityObservable = rawObservable => {
-  const signalQualityObservable = fromEvent(client, "dev");
+  const signalQualityObservable = fromEvent(client, 'dev');
   const samplingRate = 128;
   const channels = EMOTIV_CHANNELS;
   const intervalSamples = (PLOTTING_INTERVAL * samplingRate) / 1000;
@@ -112,7 +118,10 @@ export const createEmotivSignalQualityObservable = rawObservable => {
 };
 
 export const injectEmotivMarker = (value, time) => {
-  client.injectMarker({ label: "event", value, time });
+  if (sessionID !== null) {
+    client.injectMarker({ label: 'event', id: sessionID, value, time });
+  }
+  client.injectMarker({ label: 'event', value, time });
 };
 
 // ---------------------------------------------------------------------
