89bc1147e9e745651c858c4b4fbca6c2174f99da
Yury-Shevchenko <shevchenko_yury@mail.ru>
2019-08-15 16:30:53 +0200

    show behavior data (#85)
    * show behavior data
    
    * display of information

--- app/components/AnalyzeComponent.js
+++ app/components/AnalyzeComponent.js
@@ -8,7 +8,9 @@ import {
   Dropdown,
   Divider,
   Button,
-  Checkbox
+  Checkbox,
+  Transition,
+  Container
 } from 'semantic-ui-react';
 import { isNil } from 'lodash';
 import Plot from 'react-plotly.js';
@@ -66,6 +68,9 @@ interface State {
   selectedDependentVariable: string;
   removeOutliers: boolean;
   showDataPoints: boolean;
+  displayInfoVisible: boolean;
+  displayOutlierVisible: boolean;
+  displayMode: string;
   dependentVariables: Array<?{
     key: string,
     text: string,
@@ -83,9 +88,12 @@ export default class Analyze extends Component<Props, State> {
   handleBehaviorDatasetChange: (Object, Object) => void;
   handleDependentVariableChange: (Object, Object) => void;
   handleRemoveOutliers: (Object, Object) => void;
+  handleDisplayModeChange: string => void;
   handleDataPoints: (Object, Object) => void;
   saveSelectedDatasets: () => void;
   handleStepClick: (Object, Object) => void;
+  toggleDisplayInfoVisibility: () => void;
+  toggleOutlierInfoVisibility: () => void;
 
   constructor(props: Props) {
     super(props);
@@ -96,9 +104,12 @@ export default class Analyze extends Component<Props, State> {
       dependentVariables: [{ key: '', text: '', value: '' }],
       dataToPlot:[],
       layout: {},
-      selectedDependentVariable: "",
+      selectedDependentVariable: '',
       removeOutliers: false,
       showDataPoints: false,
+      displayInfoVisible: false,
+      displayOutlierVisible: false,
+      displayMode: 'datapoints',
       selectedFilePaths: [],
       selectedSubjects: [],
       selectedChannel:
@@ -111,10 +122,13 @@ export default class Analyze extends Component<Props, State> {
     this.handleBehaviorDatasetChange = this.handleBehaviorDatasetChange.bind(this);
     this.handleDependentVariableChange = this.handleDependentVariableChange.bind(this);
     this.handleRemoveOutliers = this.handleRemoveOutliers.bind(this);
+    this.handleDisplayModeChange = this.handleDisplayModeChange.bind(this);
     this.handleDataPoints = this.handleDataPoints.bind(this);
     this.saveSelectedDatasets = this.saveSelectedDatasets.bind(this);
     this.handleStepClick = this.handleStepClick.bind(this);
     this.handleDropdownClick = this.handleDropdownClick.bind(this);
+    this.toggleDisplayInfoVisibility = this.toggleDisplayInfoVisibility.bind(this);
+    this.toggleOutlierInfoVisibility = this.toggleOutlierInfoVisibility.bind(this);
   }
 
   async componentDidMount() {
@@ -160,6 +174,7 @@ export default class Analyze extends Component<Props, State> {
       this.state.selectedDependentVariable,
       this.state.removeOutliers,
       this.state.showDataPoints,
+      this.state.displayMode
     );
     this.setState({
       selectedFilePaths: data.value,
@@ -188,6 +203,7 @@ export default class Analyze extends Component<Props, State> {
       data.value,
       this.state.removeOutliers,
       this.state.showDataPoints,
+      this.state.displayMode
     );
     this.setState({
       selectedDependentVariable: data.value,
@@ -202,6 +218,7 @@ export default class Analyze extends Component<Props, State> {
       this.state.selectedDependentVariable,
       !this.state.removeOutliers,
       this.state.showDataPoints,
+      this.state.displayMode
     );
     this.setState({
       removeOutliers: !this.state.removeOutliers,
@@ -216,11 +233,42 @@ export default class Analyze extends Component<Props, State> {
       this.state.selectedDependentVariable,
       this.state.removeOutliers,
       !this.state.showDataPoints,
+      this.state.displayMode
     );
     this.setState({
       showDataPoints: !this.state.showDataPoints,
       dataToPlot: dataToPlot,
       layout: layout
+    });
+  }
+
+  handleDisplayModeChange(displayMode){
+    if(this.state.selectedFilePaths && this.state.selectedFilePaths.length > 0){
+      console.log("displayMode", displayMode)
+      const { dataToPlot, layout } = aggregateDataForPlot(
+        readBehaviorData(this.state.selectedFilePaths),
+        this.state.selectedDependentVariable,
+        this.state.removeOutliers,
+        this.state.showDataPoints,
+        displayMode
+      );
+      this.setState({
+        dataToPlot: dataToPlot,
+        layout: layout,
+        displayMode: displayMode
+      });
+    }
+  }
+
+  toggleDisplayInfoVisibility(){
+    this.setState({
+      displayInfoVisible: !this.state.displayInfoVisible
+    });
+  }
+
+  toggleOutlierInfoVisibility(){
+    this.setState({
+      displayOutlierVisible: !this.state.displayOutlierVisible
     });
   }
 
@@ -356,7 +404,18 @@ export default class Analyze extends Component<Props, State> {
                   Load datasets from different subjects and view
                   behavioral results
                 </p>
-                <Header as="h4">Select Datasets</Header>
+
+                <div>
+                  <span className="ui header">Datasets</span>
+                  <Button className='export'
+                    onClick={this.saveSelectedDatasets}
+                    >
+                    <Icon name="download" />
+                    Export
+                  </Button>
+                </div>
+                <p></p>
+
                 <Dropdown
                   fluid
                   multiple
@@ -368,14 +427,10 @@ export default class Analyze extends Component<Props, State> {
                   onChange={this.handleBehaviorDatasetChange}
                   onClick={this.handleDropdownClick}
                 />
-                <Button
-                  secondary
-                  onClick={this.saveSelectedDatasets}
-                  >
-                  Export
-                </Button>
+                <p></p>
                 <Divider hidden />
-                <Header as="h4">Select a Dependent Variable</Header>
+                <span className="ui header">Dependent Variable</span>
+                <p></p>
                 <Dropdown
                   fluid
                   selection
@@ -384,28 +439,112 @@ export default class Analyze extends Component<Props, State> {
                   options={this.state.dependentVariables}
                   onChange={this.handleDependentVariableChange}
                 />
+
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={8} style={{overflow: 'auto', maxHeight: 650 }}>
+
+              <Segment basic textAlign="left" className={styles.plotSegment} >
+                <Plot
+                  data={this.state.dataToPlot}
+                  layout={this.state.layout}
+                />
                 <p></p>
                 <Checkbox
                   checked={this.state.removeOutliers}
                   label="Remove outliers"
                   onChange={this.handleRemoveOutliers}
                 />
+                <Button className='export'
+                  onClick={this.toggleOutlierInfoVisibility}
+                  >
+                  <Icon name="info circle" />
+                </Button>
+
                 <p></p>
-                <Checkbox
-                  checked={this.state.showDataPoints}
-                  label="Show individual data points"
-                  onChange={this.handleDataPoints}
-                />
+                <Transition visible={this.state.displayOutlierVisible} animation='horizontal flip' duration={500}>
+                  <Container>
+                    <p className="info">
+                      A datapoint is tagged as an “outlier” if its value exceeds 2 standard deviations below or above the mean of all data in the group.
+                      If a datapoint is unusually high or low (it “deviates”) compared to the rest of the group,
+                      it is likely a special case that doesn’t tell us anything informative about the group as a whole.
+                      Removing such outliers can help unskew the data. What might outliers mean in your dataset?
+                      Can you think of any other cases where identifying outliers can be helpful?
+                    </p>
+                  </Container>
+                </Transition>
+
+                <p></p>
+                <Button.Group>
+                  <Button className='tertiary'
+                    toggle
+                    active={this.state.displayMode === 'datapoints'}
+                    onClick={()=> this.handleDisplayModeChange('datapoints')}
+                    >
+                    Data Points
+                  </Button>
+                  <Button className='tertiary'
+                    toggle
+                    active={this.state.displayMode === 'errorbars'}
+                    onClick={()=> this.handleDisplayModeChange('errorbars')}
+                    >
+                    Bar Graph
+                  </Button>
+                  <Button className='tertiary'
+                    toggle
+                    active={this.state.displayMode === 'whiskers'}
+                    onClick={()=> this.handleDisplayModeChange('whiskers')}
+                    >
+                    Box Plot
+                  </Button>
+                </Button.Group>
+
+                <Button className='export'
+                  onClick={this.toggleDisplayInfoVisibility}
+                  >
+                  <Icon name="info circle" />
+                </Button>
+
+                <p></p>
+                <Transition visible={this.state.displayInfoVisible && this.state.displayMode === 'datapoints'} animation='horizontal flip' duration={500}>
+                  <Container>
+                    <p className="info">
+                      In this graph, each dot refers to one data point, clustered by group (e.g., conditions).
+                      It’s the most “neutral” way of presenting the data, of course, but it may be difficult to see any patterns.
+                      Why is it always a good idea to look at all your datapoints before interpreting any trends in the data?
+                    </p>
+                  </Container>
+                </Transition>
+
+                <Transition visible={this.state.displayInfoVisible && this.state.displayMode === 'errorbars'} animation='horizontal flip' duration={500}>
+                  <Container>
+                    <p className="info">
+                      Bar graphs are the most common way to summarize data.
+                      It allows you to compare mean values between groups of datapoints (e.g., conditions),
+                      and the error bars give some indication of the variance (here: the standard error of the mean).
+                      Importantly, this way of summarizing data assumes that the mean is in fact representative of the data.
+                      Many researchers have veered away from bar graphs because they can be deceptive, especially without error bars.
+                      Can you think of any such cases?
+                    </p>
+                  </Container>
+                </Transition>
+
+                <Transition visible={this.state.displayInfoVisible && this.state.displayMode === 'whiskers'} animation='horizontal flip' duration={500}>
+                  <Container>
+                    <p className="info">
+                      Box plots summarize the data in a more informative way:
+                      they actually tell you something about the distribution of datapoints within a group,
+                      by taking the median as its reference point instead of the mean
+                      (the median is the “middle” point in a dataset after sorting it from the lowest to the highest value).
+                      The boxes represent so-called “quartiles” which are cut off at the value right between the median and the smallest value or highest value in the dataset.
+                      The lines (“whiskers”) show how much variability there is in the data outside of those quartiles;
+                      any outliers are shown as individual points. Can you go through each plot and describe exactly what you see?
+                      When you toggle between this view and the bar graph view, do the data look very different?
+                    </p>
+                  </Container>
+                </Transition>
 
               </Segment>
-            </Grid.Column>
-            <Grid.Column width={8}>
-
-              <Plot
-                data={this.state.dataToPlot}
-                layout={this.state.layout}
-              />
-
             </Grid.Column>
           </Grid>
         );
