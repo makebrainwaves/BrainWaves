35dfeba36660e44928b9b123450e46277335e751
jdpigeon <jdpigeon@hotmail.com>
2020-03-07 15:04:30 -0500

    Removed noble-winrt and patch-package

--- app/utils/eeg/muse.js
+++ app/utils/eeg/muse.js
@@ -1,54 +1,49 @@
-import 'hazardous';
-import { withLatestFrom, share, startWith, filter } from 'rxjs/operators';
+import "hazardous";
+import { withLatestFrom, share, startWith, filter } from "rxjs/operators";
 import {
   addInfo,
   epoch,
   bandpassFilter,
   addSignalQuality
-} from '@neurosity/pipes';
-import { release } from 'os';
-import { MUSE_SERVICE, MuseClient, zipSamples } from 'muse-js';
-import { from } from 'rxjs';
-import { parseMuseSignalQuality } from './pipes';
+} from "@neurosity/pipes";
+import { release } from "os";
+import { MUSE_SERVICE, MuseClient, zipSamples } from "muse-js";
+import { from } from "rxjs";
+import { webbluetooth } from "bleat";
+import { parseMuseSignalQuality } from "./pipes";
 import {
   MUSE_SAMPLING_RATE,
   MUSE_CHANNELS,
   PLOTTING_INTERVAL
-} from '../../constants/constants';
+} from "../../constants/constants";
 
 const INTER_SAMPLE_INTERVAL = -(1 / 256) * 1000;
 
 let bluetooth = {};
 let client = {};
-if (process.platform !== 'win32' || release().split('.')[0] >= 10) {
-  // Just returns the client object from Muse JS
-  bluetooth = require('bleat').webbluetooth;
-  client = new MuseClient();
-  client.enableAux = true;
+
+if (process.platform === "win32" && release().split(".")[0] < 10) {
+  console.error("Muse EEG not available in Windows 7");
 }
+
+client = new MuseClient();
+client.enableAux = false;
+
 // Gets an available Muse device
 // TODO: test whether this will ever return multiple devices if available
 export const getMuse = async () => {
   let device = {};
-  if (process.platform === 'win32') {
-    if (release().split('.')[0] < 10) {
-      console.log('win 7 ');
-      return null;
-    }
-    device = await bluetooth.requestDevice({
-      filters: [{ services: [MUSE_SERVICE] }]
-    });
-  } else {
-    device = await navigator.bluetooth.requestDevice({
-      filters: [{ services: [MUSE_SERVICE] }]
-    });
-  }
+  console.log("getting muse");
+  device = await webbluetooth.requestDevice({
+    filters: [{ services: [MUSE_SERVICE] }]
+  });
+  console.log("received ", device);
   return [device];
 };
 
 // Attempts to connect to a muse device. If successful, returns a device info object
 export const connectToMuse = async device => {
-  if (process.platform === 'win32') {
+  if (process.platform === "win32") {
     const gatt = await device.gatt.connect();
     await client.connect(gatt);
   } else {
@@ -111,10 +106,10 @@ export const injectMuseMarker = (value, time) => {
 
 const synchronizeTimestamp = (eegSample, marker) => {
   if (
-    eegSample['timestamp'] - marker['timestamp'] < 0 &&
-    eegSample['timestamp'] - marker['timestamp'] >= INTER_SAMPLE_INTERVAL
+    eegSample["timestamp"] - marker["timestamp"] < 0 &&
+    eegSample["timestamp"] - marker["timestamp"] >= INTER_SAMPLE_INTERVAL
   ) {
-    return { ...eegSample, marker: marker['value'] };
+    return { ...eegSample, marker: marker["value"] };
   }
   return eegSample;
 };
