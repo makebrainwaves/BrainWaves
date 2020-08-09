5ab998caa6a39c1585dcbdf92c8d9043619b98c2
Dano Morrison <morrisondano@gmail.com>
2020-08-09 13:16:40 -0400

    updated internals and webpack configs

--- configs/webpack.config.main.prod.babel.js
+++ configs/webpack.config.main.prod.babel.js
@@ -4,7 +4,7 @@
 
 import path from 'path';
 import webpack from 'webpack';
-import merge from 'webpack-merge';
+import { merge } from 'webpack-merge';
 import TerserPlugin from 'terser-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import baseConfig from './webpack.config.base';
@@ -14,7 +14,7 @@ import DeleteSourceMaps from '../internals/scripts/DeleteSourceMaps';
 CheckNodeEnv('production');
 DeleteSourceMaps();
 
-export default merge.smart(baseConfig, {
+export default merge(baseConfig, {
   devtool: process.env.DEBUG_PROD === 'true' ? 'source-map' : 'none',
 
   mode: 'production',
@@ -25,7 +25,7 @@ export default merge.smart(baseConfig, {
 
   output: {
     path: path.join(__dirname, '..'),
-    filename: './app/main.prod.js'
+    filename: './app/main.prod.js',
   },
 
   optimization: {
@@ -35,16 +35,16 @@ export default merge.smart(baseConfig, {
           new TerserPlugin({
             parallel: true,
             sourceMap: true,
-            cache: true
-          })
-        ]
+            cache: true,
+          }),
+        ],
   },
 
   plugins: [
     new BundleAnalyzerPlugin({
       analyzerMode:
         process.env.OPEN_ANALYZER === 'true' ? 'server' : 'disabled',
-      openAnalyzer: process.env.OPEN_ANALYZER === 'true'
+      openAnalyzer: process.env.OPEN_ANALYZER === 'true',
     }),
 
     /**
@@ -60,8 +60,8 @@ export default merge.smart(baseConfig, {
       NODE_ENV: 'production',
       DEBUG_PROD: false,
       START_MINIMIZED: false,
-      E2E_BUILD: false
-    })
+      E2E_BUILD: false,
+    }),
   ],
 
   /**
@@ -71,6 +71,6 @@ export default merge.smart(baseConfig, {
    */
   node: {
     __dirname: false,
-    __filename: false
-  }
+    __filename: false,
+  },
 });
