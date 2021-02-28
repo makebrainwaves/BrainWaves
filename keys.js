ffa067e97fab44d14d923648bfb6768015ef921a
Dano Morrison <morrisondano@gmail.com>
2021-02-27 17:33:58 -0800

    test for key presence

--- keys.js
+++ keys.js
@@ -6,5 +6,8 @@ const PASSWORD = process.env.PASSWORD;
 const CLIENT_ID = process.env.CLIENT_ID; // Created through Cortex Apps page on Emotiv.com
 const CLIENT_SECRET = process.env.CLIENT_SECRET; // Created through Cortex Apps page on Emotiv.com
 const LICENSE_ID = process.env.LICENSE_ID; // Visible on My Account page of Emotiv.com
+ 
+const envVars = { USERNAME, PASSWORD, CLIENT_ID, CLIENT_SECRET, LICENSE_ID }
+Object.keys(envVars).forEach(k => console.log(typeof envVars[k] === 'string'))
 
 module.exports = { USERNAME, PASSWORD, CLIENT_ID, CLIENT_SECRET, LICENSE_ID };
