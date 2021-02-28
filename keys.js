4bc4c918af650c8a7bd2ed1793e6730cc1e4b5de
Dano Morrison <morrisondano@gmail.com>
2021-02-27 17:20:55 -0800

    Updated env variables to match those provided through CI

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 // These  values must be filled with the appropriate Emotiv credentials to be able to use the Cortex SDK
 // We have our credentials stored in environement variables
 
-const USERNAME = process.env.EMOTIV_USERNAME;
-const PASSWORD = process.env.EMOTIV_PASSWORD;
-const CLIENT_ID = process.env.EMOTIV_CLIENT_ID; // Created through Cortex Apps page on Emotiv.com
-const CLIENT_SECRET = process.env.EMOTIV_CLIENT_SECRET; // Created through Cortex Apps page on Emotiv.com
-const LICENSE_ID = process.env.EMOTIV_LICENSE_ID; // Visible on My Account page of Emotiv.com
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+const CLIENT_ID = process.env.CLIENT_ID; // Created through Cortex Apps page on Emotiv.com
+const CLIENT_SECRET = process.env.CLIENT_SECRET; // Created through Cortex Apps page on Emotiv.com
+const LICENSE_ID = process.env.LICENSE_ID; // Visible on My Account page of Emotiv.com
 
 module.exports = { USERNAME, PASSWORD, CLIENT_ID, CLIENT_SECRET, LICENSE_ID };
