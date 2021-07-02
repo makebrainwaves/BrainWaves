f43bca6d09373c003c075ee6015d3d3c8618ecae
jdpigeon <morrisondano@gmail.com>
2021-07-02 15:15:38 -0700

    Updated pyodide actions

--- app/actions/pyodideActions.ts
+++ app/actions/pyodideActions.ts
@@ -6,8 +6,9 @@ import { PYODIDE_VARIABLE_NAMES } from '../constants/constants';
 // Actions
 
 export const PyodideActions = {
-  SendExecuteRequest: createAction<string, 'SEND_EXECUTE_REQUEST'>(
-    'SEND_EXECUTE_REQUEST'
+  Launch: createAction('LAUNCH'),
+  SetPyodideWorker: createAction<Worker, 'SET_PYODIDE_WORKER'>(
+    'SET_PYODIDE_WORKER'
   ),
   LoadEpochs: createAction<string[], 'LOAD_EPOCHS'>('LOAD_EPOCHS'),
   LoadCleanedEpochs: createAction<string[], 'LOAD_CLEANED_EPOCHS'>(
@@ -21,22 +22,13 @@ export const PyodideActions = {
     'GET_EPOCHS_INFO'
   ),
   GetChannelInfo: createAction('GET_CHANNEL_INFO'),
-  SetMainChannel: createAction<any, 'SET_MAIN_CHANNEL'>('SET_MAIN_CHANNEL'),
   SetEpochInfo: createAction<any, 'SET_EPOCH_INFO'>('SET_EPOCH_INFO'),
   SetChannelInfo: createAction<any, 'SET_CHANNEL_INFO'>('SET_CHANNEL_INFO'),
   SetPSDPlot: createAction<any, 'SET_PSD_PLOT'>('SET_PSD_PLOT'),
   SetTopoPlot: createAction<any, 'SET_TOPO_PLOT'>('SET_TOPO_PLOT'),
   SetERPPlot: createAction<any, 'SET_ERP_PLOT'>('SET_ERP_PLOT'),
-  ReceiveExecuteReply: createAction<any, 'RECEIVE_EXECUTE_REPLY'>(
-    'RECEIVE_EXECUTE_REPLY'
-  ),
-  ReceiveExecuteResult: createAction<any, 'RECEIVE_EXECUTE_RESULT'>(
-    'RECEIVE_EXECUTE_RESULT'
-  ),
-  ReceiveDisplayData: createAction<any, 'RECEIVE_DISPLAY_DATA'>(
-    'RECEIVE_DISPLAY_DATA'
-  ),
-  ReceiveStream: createAction<any, 'RECEIVE_STREAM'>('RECEIVE_STREAM'),
+  ReceiveMessage: createAction<any, 'RECEIVE_MESSAGE'>('RECEIVE_MESSAGE'),
+  ReceiveError: createAction<any, 'RECEIVE_ERROR'>('RECEIVE_ERROR'),
 } as const;
 
 export type PyodideActionType = ActionType<
