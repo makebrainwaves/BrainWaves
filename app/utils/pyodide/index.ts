8b461a8d60630ccc5a4b57dcb04b865bbbd6ab8e
jdpigeon <morrisondano@gmail.com>
2021-07-02 15:30:34 -0700

    Updated utils to typescript

--- app/utils/pyodide/index.ts
+++ app/utils/pyodide/index.ts
@@ -2,6 +2,8 @@ import * as path from 'path';
 import { readFileSync } from 'fs';
 import { formatFilePath } from './functions';
 
+declare const pyodideWorker: Worker;
+
 // ---------------------------------
 // This file contains the JS functions that allow the app to access python-wasm through pyodide
 // These functions wrap the python strings defined in the
@@ -19,19 +21,23 @@ export const loadUtils = async () =>
   });
 
 export const loadCSV = async (csvArray: Array<any>) => {
-  window.csvArray = csvArray;
   // TODO: Pass attached variable name as parameter to load_data
+  // @ts-expect-error
+  window.csvArray = csvArray;
   await pyodideWorker.postMessage({ data: `raw = load_data()` });
 };
 
 // ---------------------------
 // MNE-Related Data Processing
 
-// export const loadCleanedEpochs = (epocsArray: Array<any>) =>
-//   [
-//     `clean_epochs = concatenate_epochs([read_epochs(file) for file in files])`,
-//     `conditions = OrderedDict({key: [value] for (key, value) in clean_epochs.event_id.items()})`
-//   ].join("\n");
+export const loadCleanedEpochs = async (epochsArray: string[]) => {
+  await pyodideWorker.postMessage({
+    data: [
+      `clean_epochs = concatenate_epochs([read_epochs(file) for file in ${epochsArray}])`,
+      `conditions = OrderedDict({key: [value] for (key, value) in clean_epochs.event_id.items()})`,
+    ].join('\n'),
+  });
+};
 
 // NOTE: this command includes a ';' to prevent returning data
 export const filterIIR = async (lowCutoff: number, highCutoff: number) =>
@@ -40,10 +46,10 @@ export const filterIIR = async (lowCutoff: number, highCutoff: number) =>
   });
 
 export const epochEvents = async (
-  eventIDs: { [string]: number },
+  eventIDs: { [k: string]: number },
   tmin: number,
   tmax: number,
-  reject?: Array<string> | string = 'None'
+  reject?: string[] | 'None'
 ) =>
   pyodideWorker.postMessage({
     data: [
@@ -78,24 +84,29 @@ export const requestChannelInfo = async () =>
 
 export const cleanEpochsPlot = async () => {
   // TODO: Figure out how to get image results from pyodide
-  pyodideWorker.postMessage({
+  await pyodideWorker.postMessage({
     data: `raw_epochs.plot(scalings='auto', n_epochs=6, title="Clean Data", events=None)`,
   });
 };
 
 export const plotPSD = async () => {
   // TODO: Figure out how to get image results from pyodide
-  pyodideWorker.postMessage({ data: `raw.plot_psd(fmin=1, fmax=30)` });
+  return pyodideWorker.postMessage({ data: `raw.plot_psd(fmin=1, fmax=30)` });
 };
 
 export const plotTopoMap = async () => {
   // TODO: Figure out how to get image results from pyodide
-  pyodideWorker.postMessage({ data: `plot_topo(clean_epochs, conditions)` });
+  return pyodideWorker.postMessage({
+    data: `plot_topo(clean_epochs, conditions)`,
+  });
 };
 
-export const plotERP = (channelIndex: number) =>
-  `X, y = plot_conditions(clean_epochs, ch_ind=${channelIndex}, conditions=conditions,
-    ci=97.5, n_boot=1000, title='', diff_waveform=None)`;
+export const plotERP = async (channelIndex: number) => {
+  return pyodideWorker.postMessage({
+    data: `X, y = plot_conditions(clean_epochs, ch_ind=${channelIndex}, conditions=conditions,
+      ci=97.5, n_boot=1000, title='', diff_waveform=None)`,
+  });
+};
 
 export const saveEpochs = (workspaceDir: string, subject: string) =>
   pyodideWorker.postMessage({
